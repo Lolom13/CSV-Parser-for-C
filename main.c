a2a40ac3eac1a525c0253f84d91d464e3b2ae316
_Lolom <louis.miermont@laposte.net>
2020-01-14 18:26:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include "utils.cuh"
 
-#define DATA_NROWS	34
-#define DATA_NCOLS	7
-#define MAXSIZE_FIELDSNAMES	32
+#define DATA_NROWS 34
+#define DATA_NCOLS 7
+#define MAXSIZE_FIELDSNAMES 32
 #define SIZE 1024
 
 
@@ -21,7 +21,6 @@ int main()
 	for (i = 0; i < DATA_NCOLS; i++)
 		fields_names[i] = (char*)malloc((MAXSIZE_FIELDSNAMES + 1) * sizeof(char));
 
-	
 	readCSV(DATA_NROWS, DATA_NCOLS, ";", "cars.csv", data, fields_names);
 
 	return 0;
